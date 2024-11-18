98a806a20267a401c85da47bce38094b12b9abbd
氏家憂也 <yuya61ujiie@ujiieyuya.local>
2024-11-18 20:06:29 +0900

    model spotのactive storage対応の追記

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -5,6 +5,7 @@ class Spot < ApplicationRecord
   validates :body, presence: true, length: { maximum: 65_535 }
 
   belongs_to :user
+  has_one_attached :image
 
   enum category: { cafe: 0, karaoke: 1 }
 
