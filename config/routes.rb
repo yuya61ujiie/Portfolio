3216aba4b60a018df0c7a01fc394de3e52d9685e
氏家憂也 <yuya61ujiie@ujiieyuya.local>
2024-11-25 19:19:38 +0900

    refactor:ルーティングの順序を入れ替える

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
   root "static_pages#top"
   resources :spots
+  resources :comments
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
