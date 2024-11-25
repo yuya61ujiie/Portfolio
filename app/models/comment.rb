d169f4726a76e9ff468af6f3b3a21899c38a6edc
氏家憂也 <yuya61ujiie@ujiieyuya.local>
2024-11-25 19:12:11 +0900

    feat:model comment バリデーション、リレーション、enumの設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,19 @@
 class Comment < ApplicationRecord
+  validates :scene, presence: true
+  validates :start_at, presence: true
+  validates :finish_at, presence: true
+  validate :start_finish_check
+  validates :rating, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, presence: true, length: { maximum: 65_535 }
+
+	def start_finish_check
+    errors.add(:finish_time, "は開始時刻より遅い時間を選択してください") if self.start_time > self.finish_time
+  end
+
+  belongs_to :user
+  belongs_to :spot
+
+  enum :scene { pc_work: 0, manual_work: 1, reading: 2 }
+  enum :rating { 0.5: 0, 1.0: 1, 1.5: 2, 2.0: 3, 2.5: 4, 3.0: 5 }
 end
