ac0df26e15b2935cc9e34eeb9e9a4d76e10835a5
氏家憂也 <yuya61ujiie@ujiieyuya.local>
2024-11-26 19:02:16 +0900

    fix:commentフォームの修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,14 +2,14 @@ class Comment < ApplicationRecord
   validates :scene, presence: true
   validates :start_at, presence: true
   validates :finish_at, presence: true
-  validate :start_finish_check
+#  validate :start_finish_check
   validates :rating, presence: true
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
 
-	def start_finish_check
-    errors.add(:finish_time, "は開始時刻より遅い時間を選択してください") if self.start_time > self.finish_time
-  end
+#	def start_finish_check
+#    errors.add(:finish_at, "は開始時刻より遅い時間を選択してください") if self.start_at > self.finish_at
+#  end
 
   belongs_to :user
   belongs_to :spot
