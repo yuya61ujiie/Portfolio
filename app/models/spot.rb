a045bbfd13b67882ceaf90608869cdeba4177fa0
yuya61ujiie <yuya_ujiie@yahoo.co.jp>
2024-12-08 17:57:51 +0900

    change: imageのvaridationを追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,6 +3,8 @@ class Spot < ApplicationRecord
   validates :category, presence: true
   validates :address, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
+  validate :image_content_type
+  validate :image_size
 
   belongs_to :user
   has_many :comments, dependent: :destroy
@@ -10,4 +12,17 @@ class Spot < ApplicationRecord
   has_one_attached :image
 
   enum category: { cafe: 1, work_space: 2, karaoke: 3, other: 4 }
+
+  def image_content_type
+    if image.attached? && !image.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:image, '：ファイル形式が、JPEG, PNG, GIF以外になっています。ファイル形式をご確認ください。')
+    end
+  end
+
+  def image_size
+    if image.attached? && image.blob.byte_size > 1.megabytes
+      errors.add(:image, '：1MB以下のファイルをアップロードしてください。')
+    end
+  end
+
 end
