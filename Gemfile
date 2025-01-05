95c26a7ceaad1ae250e361c51044627b92ae91d2
yuya61ujiie <yuya_ujiie@yahoo.co.jp>
2025-01-05 11:00:55 +0900

    style:Lint修正

--- Gemfile
+++ Gemfile
@@ -61,7 +61,7 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
 
-  gem 'letter_opener_web'
+  gem "letter_opener_web"
 end
 
 group :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,15 @@ Rails.application.configure do
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://yuya61ujiie-portfolio-bcd33720e946.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: "https://yuya61ujiie-portfolio-bcd33720e946.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
+      address:              "smtp.gmail.com",
       enable_starttls_auto: true,
       port:                 587,
-      domain:               'gmail.com',
-      user_name:            ENV['GMAIL_USER'],
-      password:             ENV['GMAIL_PASS'],
+      domain:               "gmail.com",
+      user_name:            ENV["GMAIL_USER"],
+      password:             ENV["GMAIL_PASS"],
       authentication:       :plain
     }
 
