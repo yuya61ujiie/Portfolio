bb5acaf16d82574b0d5743e822a73105e225c74a
yuya61ujiie <yuya_ujiie@yahoo.co.jp>
2025-01-21 16:27:44 +0900

    feat: @spotが存在する場合には、OGPにスポット画像が表示されるように変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,33 +1,64 @@
 module ApplicationHelper
 
-  def default_meta_tags
-    {
-      site: 'Morning Hub',
-      title: '朝活スポット検索アプリ - Morning Hub',
-      reverse: true,
-      charset: 'utf-8',
-      description: '朝活を充実させたい方必見！カフェ、ワークスペース、公園など全国の朝活スポットを簡単に検索できるアプリ。理想の朝をスタートさせましょう。',
-      keywords: '朝活, スポット検索, カフェ, ワークスペース, 公園, 朝の活動',
-      canonical: request.original_url,
-      separator: '|',
-      icon: [
-        { href: image_url("favicon.ico")},
-        { href: image_url("favicon.png"), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
-      ],
-      og: {
-        site_name: :site,
-        title: :title,
-        description: :description,
-        type: 'website',
-        url: request.original_url,
-        image: image_url('top_image.png'),
-        local: 'ja-JP',
-      },
-      twitter: {
-        card: 'summary_large_image',
-        site: '@yuya_ujiie',
-        image: image_url('top_image.png'),
+  def assign_meta_tags
+    if @spot.nil?
+      {
+        site: 'Morning Hub',
+        title: '朝活スポット検索アプリ - Morning Hub',
+        reverse: true,
+        charset: 'utf-8',
+        description: '朝活を充実させたい方必見！カフェ、ワークスペース、公園など全国の朝活スポットを簡単に検索できるアプリ。理想の朝をスタートさせましょう。',
+        keywords: '朝活, スポット検索, カフェ, ワークスペース, 公園, 朝の活動',
+        canonical: request.original_url,
+        separator: '|',
+        icon: [
+          { href: image_url("favicon.ico")},
+          { href: image_url("favicon.png"), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+        ],
+        og: {
+          site_name: :site,
+          title: :title,
+          description: :description,
+          type: 'website',
+          url: request.original_url,
+          image: image_url('top_image.png'),
+          local: 'ja-JP',
+        },
+        twitter: {
+          card: 'summary_large_image',
+          site: '@yuya_ujiie',
+          image: image_url('top_image.png'),
+        }
       }
-    }
+    else
+      {
+        site: 'Morning Hub',
+        title: "#{@spot.spot_name}",
+        reverse: true,
+        charset: 'utf-8',
+        description: "#{@spot.body}",
+        keywords: '朝活, スポット検索, カフェ, ワークスペース, 公園, 朝の活動',
+        canonical: request.original_url,
+        separator: '|',
+        icon: [
+          { href: image_url("favicon.ico")},
+          { href: image_url("favicon.png"), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+        ],
+        og: {
+          site_name: :site,
+          title: :title,
+          description: :description,
+          type: 'website',
+          url: request.original_url,
+          image: image_url(url_for(@spot.image)),
+          local: 'ja-JP',
+        },
+        twitter: {
+          card: 'summary_large_image',
+          site: '@yuya_ujiie',
+          image: image_url(url_for(@spot.image)),
+        }
+      }
+    end
   end
 end
