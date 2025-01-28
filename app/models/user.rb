931b2f2f0529a8a7796c3002b9a85f67876c1c15
yuya61ujiie <yuya_ujiie@yahoo.co.jp>
2025-01-28 14:45:34 +0900

    feat: userにomniauthの設定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+          :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :spots
   has_many :comments
