161ee5800994abad845dd9688530a0e225cdf5f1
yuya61ujiie <yuya_ujiie@yahoo.co.jp>
2025-01-28 14:24:57 +0900

    feat: usersテーブルにomniauthのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_10_014458) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_28_052053) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -101,7 +101,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_10_014458) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
