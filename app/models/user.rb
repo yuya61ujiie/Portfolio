50e0ac1618bdcdfe79a1c2aa55a8abd622c87ddd
yuya61ujiie <yuya_ujiie@yahoo.co.jp>
2025-01-29 10:01:03 +0900

    feat: omniauth_callbacks_controller生成、使用するクラスメソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+  devise  :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
           :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :spots
@@ -23,5 +23,19 @@ class User < ApplicationRecord
 
   def bookmark?(spot)
     bookmark_spots.include?(spot)
+  end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+        user = User.create(
+          name: data['name'],
+          email: data['email'],
+          password: Devise.friendly_token[0,20]
+        )
+    end
+    user
   end
 end
