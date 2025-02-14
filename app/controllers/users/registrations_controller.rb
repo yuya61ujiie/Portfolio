245773cb29775274f597adf2a35ce8afb2033ecc
yuya61ujiie <yuya_ujiie@yahoo.co.jp>
2025-02-14 19:13:33 +0900

    feat: プロフィール更新時にプロフィール画面にリダイレクト

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -63,4 +63,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
+
+  def after_update_path_for(resource)
+    users_profile_path
+  end
 end
