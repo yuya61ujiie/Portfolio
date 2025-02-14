9e1a7e9ee0880110b3a1cf2d556d7564ea4dd0c8
yuya61ujiie <yuya_ujiie@yahoo.co.jp>
2025-02-14 18:32:27 +0900

    feat: プロフィール変更時のストロングパラメータ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :configure_sign_up_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-    def configure_sign_up_parameters
+    def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+      devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
     end
 end
